eed15875317f71d4bed1093264ce202d5d92fea2
navznnz <nataliaviviana22@gmail.com>
2025-08-19 09:40:48 -0500

    Create Solve.java

---
+++ Solve.java
@@ -0,0 +1,7 @@
+public class Solve {
+    public static void main(String[] args){
+        boolean a = false;
+        for (int i=1; !a; i++){
+            if (i%5 ==0){
+                a= true;
+            }}}}
