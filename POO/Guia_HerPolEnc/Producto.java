1f2ed55dd22669c624a3f9a35bd8347d8d2f3768
Natalia Z <nataliaviviana22@gmail.com>
2025-09-29 19:29:01 -0500

    Guardar cambios finales antes de pull

--- POO/Guia_HerPolEnc/Producto.java
+++ POO/Guia_HerPolEnc/Producto.java
@@ -11,30 +11,19 @@ public abstract class Producto {
     }
 
     // Getters y Setters (Encapsulamiento)
-    public String getNombre() {
-        return nombre;
-    }
+    public String getNombre() { return nombre; }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+    public void setNombre(String nombre) { this.nombre = nombre;}
 
-    public double getPrecio() {
-        return precio;
-    }
+    public double getPrecio() {  return precio;}
 
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
+    public void setPrecio(double precio) { this.precio = precio; }
 
-    public int getStock() {
-        return stock;
-    }
+    public int getStock() { return stock; }
 
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
+    public void setStock(int stock) { this.stock = stock; }
 
     // Polimorfismo: método a sobrescribir
     public abstract String mostrarInfo();
 }
+
